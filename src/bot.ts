77aa0faf7006ee62fae7125296f2d862e6a06c50
Sophie Amity Debs <sophie.a.debs@gmail.com>
2026-07-05 23:05:15 -0700

    update to post every 12 horus instead of every 4

--- src/bot.ts
+++ src/bot.ts
@@ -6,7 +6,7 @@ import { randomInt } from 'node:crypto';
 
 import lines from './lines.js';
 
-const INTERVAL_MS = 4 * 60 * 60 * 1000; // 4 hours
+const INTERVAL_MS = 12 * 60 * 60 * 1000; // 4 hours
 // let interval: NodeJS.Timeout | null = null;
 
 dotenv.config();
